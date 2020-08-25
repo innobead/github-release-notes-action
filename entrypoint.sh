801a38ab11321bd7f5d796ccfff95fc76dc2eb35
David Ko <dko@suse.com>
2020-08-26 03:01:48 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@ IFS='/' read -r username repository <<EOF
 $GITHUB_REPOSITORY
 EOF
 
-GREN_GITHUB_TOKEN=$GITHUB_TOKEN gren release --username=$username --repo=$repository
+GREN_GITHUB_TOKEN=$GITHUB_TOKEN gren changelog --username=$username --repo=$repository
