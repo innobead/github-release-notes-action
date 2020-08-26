5ba9afbd52cc335d5c4278435fa3ab386c5cb066
David Ko <dko@suse.com>
2020-08-26 10:38:48 +0800

    Use draft release with a specific tag

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@ IFS='/' read -r username repository <<EOF
 $GITHUB_REPOSITORY
 EOF
 
-GREN_GITHUB_TOKEN=$GITHUB_TOKEN gren changelog --username=$username --repo=$repository
+tag_name="${GITHUB_REF##*/}"
+GREN_GITHUB_TOKEN=$GITHUB_TOKEN gren release -d --username="$username" --repo="$repository" --tags="${tag_name}"
 
 changelog=$(cat ./CHANGELOG.md)
 echo "::set-output name=changelog::${changelog}"
